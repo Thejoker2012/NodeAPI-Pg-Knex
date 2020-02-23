59542b7aa3f5055a06cfcc10a9dcc92570d530d8
Iago Oliveira Santos <iagofalcom@hotmail.com>
2020-02-23 15:59:44 -0400

    dotenv variáveis de ambiente

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,4 +1,7 @@
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 module.exports = (app) =>{
     app.use(bodyParser.json());
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const app = require('./app.js');
 
-const PORT=3001
+//Variável de ambiente PORT
+const PORT=process.env.PORT || 3001
 
 app.listen(PORT, ()=>{
     console.log(`Servidor rodando na porta ${PORT}`);
