b015adefe55baf0139dc069a84a6f5d768f0d804
Iago Oliveira Santos <iagofalcom@hotmail.com>
2020-02-23 11:33:02 -0400

    Teste listar usuários

--- src/app.js
+++ src/app.js
@@ -6,4 +6,11 @@ app.get('/', (req, res)=>{
     res.status(200).send();
 })
 
+app.get('/users', (req, res)=>{
+    const users = [
+        {name: 'Iago', email:'iago@email.com'}
+    ]
+    res.status(200).json(users);
+})
+
 module.exports = app;
