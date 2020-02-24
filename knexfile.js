db08234430b300b515257fcc496c4d1b2b1a089a
Iago Oliveira Santos <iagofalcom@hotmail.com>
2020-02-23 23:48:01 -0400

    knex .env

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,17 @@
+var dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
+    
     test: {
         client: 'pg',
         version: '9.6',
+        
         connection: {
-            host:'localhost',
-            user:'postgres',
-            password:'admin',
-            database:'barriga'
+            host:process.env.KNEX_HOST,
+            user:process.env.KNEX_USER,
+            password:process.env.KNEX_PASSWORD,
+            database:process.env.KNEX_DATABASE
         },
 
         migrations: {
