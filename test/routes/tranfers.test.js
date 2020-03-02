f38a8bf19843cf0be59523dd82d5fa3ecd899391
Iago Oliveira Santos <iagofalcom@hotmail.com>
2020-03-02 13:28:51 -0400

    Inserir trnferência com sucesso
    Router
    Service

--- test/routes/tranfers.test.js
+++ test/routes/tranfers.test.js
@@ -26,3 +26,28 @@ test('Deve listar apenas as tranferencias do usuário', ()=>{
     });
 
 });
+
+test('Deve inserir uma tranferencia com sucesso', ()=>{
+    return request(app).post(MAIN_ROUTE)
+        .set('authorization', `bearer ${TOKEN}`)
+        .send({description:'Regular Transfers', user_id: 10000, acc_ori_id:10000, acc_dest_id:10001, ammount:100, date:new Date()})
+        .then(async (res)=>{
+            //console.log(res.body)
+            expect(res.status).toBe(201);
+            expect(res.body.description).toBe('Regular Transfers');
+            
+            const transactions = await app.db('transactions').where({transfer_id: res.body.id})
+            expect(transactions).toHaveLength(2);
+            expect(transactions[0].description).toBe('Transfer to acc #10001');
+            expect(transactions[1].description).toBe('Transfer from acc #10000');
+            expect(transactions[0].ammount).toBe('-100.00');
+            expect(transactions[1].ammount).toBe('100.00');
+            expect(transactions[0].acc_id).toBe(10000);
+            expect(transactions[1].acc_id).toBe(10001);
+
+
+
+
+
+        });
+});
