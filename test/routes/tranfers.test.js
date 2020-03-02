fbd60ccaea7271122eb11f646a4585031e947663
Iago Oliveira Santos <iagofalcom@hotmail.com>
2020-03-02 13:58:49 -0400

    Test Tranfers
    Retornar 201 ao salvar
    Transações geradas
    Saida deve ser negativa
    Entrada deve ser positiva
    Saida e entrada devem referenciar a origem

--- test/routes/tranfers.test.js
+++ test/routes/tranfers.test.js
@@ -44,10 +44,49 @@ test('Deve inserir uma tranferencia com sucesso', ()=>{
             expect(transactions[1].ammount).toBe('100.00');
             expect(transactions[0].acc_id).toBe(10000);
             expect(transactions[1].acc_id).toBe(10001);
-
-
-
-
-
         });
 });
+
+describe('Ao salvar uma transferência válida...', ()=>{
+    let transferId;
+    let income;
+    let outcome;
+    
+    test('Deve retornar o status 201 e os dados da tranferência', ()=>{
+        return request(app).post(MAIN_ROUTE)
+        .set('authorization', `bearer ${TOKEN}`)
+        .send({description:'Regular Transfers', user_id: 10000, acc_ori_id:10000, acc_dest_id:10001, ammount:100, date:new Date()})
+        .then(async (res)=>{
+            console.log(res.body)
+            expect(res.status).toBe(201);
+            expect(res.body.description).toBe('Regular Transfers');
+            transferId = res.body.id;
+        });
+    });
+
+    test('As transações equivalentes devem ter sido geradas', async ()=>{
+        const transactions = await app.db('transactions').where({transfer_id: transferId}).orderBy('ammount')
+        expect(transactions).toHaveLength(2);
+        [outcome, income] = transactions;
+    });
+
+    test('A transação de saida deve ser negativa', ()=>{
+        expect(outcome.description).toBe('Transfer to acc #10001');
+        expect(outcome.ammount).toBe('-100.00');
+        expect(outcome.acc_id).toBe(10000);
+        expect(outcome.type).toBe('O');
+    });
+
+    test('A transação de entrada deve ser positiva', ()=>{
+        expect(income.description).toBe('Transfer from acc #10000');
+        expect(income.ammount).toBe('100.00');
+        expect(income.acc_id).toBe(10001);
+        expect(income.type).toBe('I');
+
+    });
+
+    test('Ambas devem referenciar a tranferencia que as originou', ()=>{
+        expect(income.transfer_id).toBe(transferId);
+        expect(outcome.transfer_id).toBe(transferId);
+    });
+})
