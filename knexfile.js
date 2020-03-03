ebb1767fe17b75d000d19cd21fa29e6f2cc93b08
Iago Oliveira Santos <iagofalcom@hotmail.com>
2020-03-03 13:19:05 -0400

    Configurando ambiente de produção

--- knexfile.js
+++ knexfile.js
@@ -6,23 +6,34 @@ module.exports = {
     test: {
         client: 'pg',
         version: '9.6',
-        
         connection: {
             host:process.env.KNEX_HOST,
             user:process.env.KNEX_USER,
             password:process.env.KNEX_PASSWORD,
             database:process.env.KNEX_DATABASE
         },
-
         //Diretório onde ficaram as tabelas migrations
         migrations: {
             directory:'src/migration',
         },
-
         //Diretório para arquivos de popular as tabelas
         seeds: {
             directory:'src/seeds',
         }
+    },
+    prod: {
+        client: 'pg',
+        version: '9.6',
+        connection: {
+            host:process.env.KNEX_HOST,
+            user:process.env.KNEX_USER,
+            password:process.env.KNEX_PASSWORD,
+            database:process.env.KNEX_DATABASE
+        },
+        //Diretório onde ficaram as tabelas migrations
+        migrations: {
+            directory:'src/migration',
+        },
     },
 };
 
