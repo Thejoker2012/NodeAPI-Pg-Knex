1a7fdcde6518e782e10e7ca83f17bced3df32784
Iago Oliveira Santos <iagofalcom@hotmail.com>
2020-03-03 14:25:31 -0400

    Instalando CORS
    Dependencia para acessar a api via clientes como react, angular entre outro s.

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 //log de migrations
 const knexlogger = require('knex-logger');
+const cors = require('cors')
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ module.exports = (app) =>{
     
     app.use(bodyParser.json());
     app.use(knexlogger(app.db));
+    app.use(cors({origin:'*'}))
     
 }
 
